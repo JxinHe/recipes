3db8f73b22382e8ea4c4786809afb701aea6847b
JxinHe <40479291+JxinHe@users.noreply.github.com>
2019-03-03 21:20:50 +0800

    Update EventLoop.h

--- reactor/s00/EventLoop.h
+++ reactor/s00/EventLoop.h
@@ -1,45 +1,40 @@
-// excerpts from http://code.google.com/p/muduo/
-//
-// Use of this source code is governed by a BSD-style license
-// that can be found in the License file.
-//
-// Author: Shuo Chen (chenshuo at chenshuo dot com)
-
 #ifndef MUDUO_NET_EVENTLOOP_H
 #define MUDUO_NET_EVENTLOOP_H
 
-#include "thread/Thread.h"
+#include"thread/Thread.h"
 
-namespace muduo
+namesapce muduo
 {
 
-class EventLoop : boost::noncopyable
-{
+ class EventLoop:boost::noncopyable
+ {
  public:
 
-  EventLoop();
-  ~EventLoop();
+ EventLoop();
+ ~EventLoop();
 
-  void loop();
+ void loop();
 
-  void assertInLoopThread()
+ void asserInLoopThread()//检查当前线程是否创建了其他EventLoop对象
+ {
+  if(!isInLoopThread())
   {
-    if (!isInLoopThread())
-    {
-      abortNotInLoopThread();
-    }
+   abortNotInLoopThread();
   }
-
-  bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
-
+ }
+ 
+ bool isInLoopThread()const
+ {
+  return threadId_==CurrentThread::tid()；
+ }
+ 
  private:
-
+  bool looping_; //原子操作
+  
   void abortNotInLoopThread();
-
-  bool looping_; /* atomic */
   const pid_t threadId_;
+ 
 };
 
 }
-
-#endif  // MUDUO_NET_EVENTLOOP_H
+#endif //MUDUO_NET_EVENTLOOP_H
