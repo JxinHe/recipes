785be68e40e50d664600cd3314080e3458dd1847
JxinHe <40479291+JxinHe@users.noreply.github.com>
2019-03-03 22:29:47 +0800

    Update Channel.cc

--- reactor/s01/Channel.cc
+++ reactor/s01/Channel.cc
@@ -1,51 +1,47 @@
-// excerpts from http://code.google.com/p/muduo/
-//
-// Use of this source code is governed by a BSD-style license
-// that can be found in the License file.
-//
-// Author: Shuo Chen (chenshuo at chenshuo dot com)
+#include"Channel.h"
+#include"EventLoop.h"
+#include"logging/Logging.h"
 
-#include "Channel.h"
-#include "EventLoop.h"
-#include "logging/Logging.h"
+#include<sstream>
 
-#include <sstream>
-
-#include <poll.h>
+#include<poll.h>
 
 using namespace muduo;
 
-const int Channel::kNoneEvent = 0;
-const int Channel::kReadEvent = POLLIN | POLLPRI;
-const int Channel::kWriteEvent = POLLOUT;
+const int Channel::kNoneEvent=0;//不关注的事件
+const int Channel::kReadEvent=POLLIN|POLLPRI;//关注的读事件
+const int Channel::kWriteEvent=POLLOUT;
 
-Channel::Channel(EventLoop* loop, int fdArg)
-  : loop_(loop),
-    fd_(fdArg),
-    events_(0),
-    revents_(0),
-    index_(-1)
-{
-}
-
+Channel::Channel(EventLoop* loop,int fdArg)
+  :loop_(loop),fd_(fdArg),events_(0),revents_(0),index_(-1)
+  {
+    
+  }
 void Channel::update()
 {
-  loop_->updateChannel(this);
+  loop_->updateChannel(this);//会转而调用Poller::updateChannel() 
 }
 
-void Channel::handleEvent()
+
+void Channel::handleEvent()//核心，有Eventloop::loop()调用，根据不同revents_值调用不同用户回调
 {
-  if (revents_ & POLLNVAL) {
-    LOG_WARN << "Channel::handle_event() POLLNVAL";
+   if(revents_&POLLNVAL)
+     LOG_WARN<<"Channel::handle_event() PLOONVAL";
+  
+   if(revents_&()POLLERR|POLLNVAL)
+   {
+      if(errorCallback_)
+        errorCallback_();
+   }
+  if(revents_&(POLLING|POLLPRI|POLLRDHUP))
+  {
+    if(readCallback_)
+      readCallback_();
   }
-
-  if (revents_ & (POLLERR | POLLNVAL)) {
-    if (errorCallback_) errorCallback_();
-  }
-  if (revents_ & (POLLIN | POLLPRI | POLLRDHUP)) {
-    if (readCallback_) readCallback_();
-  }
-  if (revents_ & POLLOUT) {
-    if (writeCallback_) writeCallback_();
+  if(revents_&POLLOUT)
+  {
+   if(writeCallback_)
+     writeCallback_();
   }
 }
+
