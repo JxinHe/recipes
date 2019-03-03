0ae0b0298684f449d8f0ac57cdcd64d17663e273
JxinHe <40479291+JxinHe@users.noreply.github.com>
2019-03-03 21:35:45 +0800

    Update EventLoop.cc

--- reactor/s00/EventLoop.cc
+++ reactor/s00/EventLoop.cc
@@ -1,59 +1,47 @@
-// excerpts from http://code.google.com/p/muduo/
-//
-// Use of this source code is governed by a BSD-style license
-// that can be found in the License file.
-//
-// Author: Shuo Chen (chenshuo at chenshuo dot com)
+#include"EventLoop.h"
 
-#include "EventLoop.h"
+#include"logging/Logging.h"
 
-#include "logging/Logging.h"
+#include<assert.h>
+#include<poll.h>
 
-#include <assert.h>
-#include <poll.h>
+using name sapce muduo;
 
-using namespace muduo;
+__thread EventLoop* t_loopInThisThread=0;
 
-__thread EventLoop* t_loopInThisThread = 0;
-
-EventLoop::EventLoop()
-  : looping_(false),
-    threadId_(CurrentThread::tid())
+EventLoop::EventLoop()：looping(false),threadId_(CurrentThread::tid())
 {
-  LOG_TRACE << "EventLoop created " << this << " in thread " << threadId_;
-  if (t_loopInThisThread)
-  {
-    LOG_FATAL << "Another EventLoop " << t_loopInThisThread
-              << " exists in this thread " << threadId_;
+  if(t_loopInThisThread)
+  {  
+    LOG_FATAL<<"Another EventLoop"<<this<<"in thread"<<threadId_; 
   }
   else
   {
-    t_loopInThisThread = this;
+    t_loopInThisThread=this;
   }
 }
 
 EventLoop::~EventLoop()
 {
   assert(!looping_);
-  t_loopInThisThread = NULL;
+  t_loopInThisThread=NULL;
 }
 
 void EventLoop::loop()
 {
   assert(!looping_);
   assertInLoopThread();
-  looping_ = true;
-
-  ::poll(NULL, 0, 5*1000);
-
-  LOG_TRACE << "EventLoop " << this << " stop looping";
-  looping_ = false;
-}
+  looping_=true;
+  
+  ::poll(NULL,0,5*1000);
+  
+  LOG_TRACE<<"EventLoop"<<this<<"stop looping";
+  looping_=false;
+} 
 
 void EventLoop::abortNotInLoopThread()
 {
-  LOG_FATAL << "EventLoop::abortNotInLoopThread - EventLoop " << this
-            << " was created in threadId_ = " << threadId_
-            << ", current thread id = " <<  CurrentThread::tid();
+  LOG_FATAL<<"EventLoop::abortNotInLoopThread-EventLoop"<<this
+    <<"was created in threadId_="<<threadId_<<",current thread id="<<CurrentThread::tid();
 }
-
+}
